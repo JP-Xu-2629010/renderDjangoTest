607213976820842f50720a11fdadec1f131c8b59
jxu210 <j3.xu@student.vu.nl>
2024-05-14 23:07:42 +0200

    init db

--- tree01/models.py
+++ tree01/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+# from django.contrib.gis.db import models as gis_models
 
 # Create your models here.
+class UserCase(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    tree_type = models.IntegerField(choices=[(i, str(i)) for i in range(9)])
+    location = models.CharField(max_length=32)
+    audio_file = models.FileField(upload_to='audio_files/')
